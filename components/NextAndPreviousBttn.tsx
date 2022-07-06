9b05bb07a0ca809a38f9f0c1397e702bd308e3ac
Saurabh <raisaurabh265@gmail.com>
2022-07-07 03:40:15 +0530

    new component made for buttons and styles changed

---
+++ components/NextAndPreviousBttn.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function NextAndPreviousBttn({
+  page,
+  setPage,
+  setNextPage,
+  findPage,
+  data,
+}: any) {
+  return (
+    <div className="text-center space-x-4 flex justify-center px-2 sm:px-0">
+      <button
+        onClick={() => setPage(findPage().prev)}
+        disabled={page === 1}
+        style={{
+          cursor: page === 1 ? "not-allowed" : "pointer",
+        }}
+        className="border-2 text-white bg-[#953ffa] border-[#953ffa] p-1 rounded-md"
+      >
+        Previous Page
+      </button>
+
+      <p className="text-center">
+        <span className="text-white">Showing Page </span>
+        <span className="text-[#953ffa]">{findPage().current} </span>
+        <span className="text-white">
+          out of {data && data.characters.info.pages}
+        </span>
+      </p>
+      <button
+        onClick={setNextPage}
+        className="border-2 text-white bg-[#953ffa] border-[#953ffa] p-1 rounded-md"
+        disabled={page === data && data.characters.info.pages}
+      >
+        Next Page
+      </button>
+    </div>
+  );
+}
+
+export default NextAndPreviousBttn;
